562ffb13919730802e50d2548ba71108154afead
Algis <algis.h@yandex.ru>
2021-09-29 20:45:58 +0300

    Выполнена домашняя работа №2.

---
+++ homework_2/function.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "function.cpp"
+
+template<typename T>
+void swap(T, T);
+
+void FillVector(std::vector<int*> &, unsigned int);
+
+void PrintVector(std::vector<int*> &);
+
+void SortPoiner(std::vector<int*> &);
+
+int FindVowel_a(std::ifstream &);
+
+int FindVowel_b(std::ifstream &);
+
+int FindVowel_c(std::ifstream &);
+
+int FindVowel_d(std::ifstream &);
